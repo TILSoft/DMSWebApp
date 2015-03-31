3ec1c8159c88b22f58a65bec859d984936896cf7
dinnoc <james.dinniss@gmail.com>
2015-03-31 12:50:04 +0100

    Clean up

---
+++ zAdministration/AccessDenied.aspx
@@ -0,0 +1,9 @@
+﻿<%@ Page Language="VB" MasterPageFile="~/FrontEnd.master" AutoEventWireup="false" CodeFile="AccessDenied.aspx.vb" Inherits="zAdministration_AccessDenied" title="Untitled Page" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <p>
+        You do not have the access rights for this page</p>
+</asp:Content>
+
---
+++ zAdministration/EmailByLine.aspx.vb
@@ -0,0 +1,27 @@
+﻿
+Partial Class zAdministration_EmailByLine
+    Inherits System.Web.UI.Page
+
+    Protected Sub Button1_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim adaplineemail As New UsersTableAdapters.tblEmailbyLineTableAdapter
+        adaplineemail.Insert(DropDownList1.SelectedValue, RadioButtonList1.SelectedValue)
+        GridView1.DataBind()
+
+    End Sub
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Dim hlp As New Helpers
+        If hlp.checkloginstatus(User.Identity.Name, 4) <= 0 Then
+            Response.Redirect("AccessDenied.aspx")
+        End If
+
+        Dim hsAdap As New ConfigTableAdapters.QueriesTableAdapter
+
+        lblHandShake.Text = hsAdap.DateValFromConfigID(3)
+
+
+
+
+
+    End Sub
+End Class
